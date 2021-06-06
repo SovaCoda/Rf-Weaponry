6e7eacf9aa8a3eaa72ab25084c669750b8d95787
SovaCoda <80545648+SovaCoda@users.noreply.github.com>
2021-06-05 23:45:03 -0500

    new base projectile class

--- src/main/java/com/sovacoda/rfweaponry/client/render/entity/LaserPistolIShotRenderer.java
+++ src/main/java/com/sovacoda/rfweaponry/client/render/entity/LaserPistolIShotRenderer.java
@@ -1,5 +1,7 @@
 package com.sovacoda.rfweaponry.client.render.entity;
 
+import java.util.Random;
+
 import org.lwjgl.opengl.GL11;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
@@ -9,16 +11,28 @@ import com.sovacoda.rfweaponry.client.render.model.LaserPistolIShotModel;
 import com.sovacoda.rfweaponry.common.entities.LaserEntity;
 import com.sovacoda.rfweaponry.common.entities.LaserPistolIShotEntity;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Matrix3f;
 import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Quaternion;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.vector.Vector3f;
+import net.minecraftforge.client.model.data.EmptyModelData;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoProjectilesRenderer;
@@ -30,11 +44,25 @@ public class LaserPistolIShotRenderer extends GeoProjectilesRenderer<LaserPistol
 		this.shadowRadius = 0.7f;
 	}
 	
+    private void add(IVertexBuilder renderer, MatrixStack stack, float x, float y, float z, float u, float v) {
+        renderer.vertex(stack.last().pose(), x, y, z)
+                .color(1.0f, 1.0f, 1.0f, 1.0f)
+                .uv(u, v)
+                .uv2(0, 240)
+                .normal(1, 0, 0)
+                .endVertex();
+    }
+    
+
+	
 	@Override
 	public void render(LaserPistolIShotEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn,
 			IRenderTypeBuffer bufferIn, int packedLightIn) {
 		super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
-		Tessellator tessellator = Tessellator.getInstance();
+		
+       
+		
+		
 	}
 
 
